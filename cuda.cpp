e945ba1c052b3dc88ff6e78a8dedd6f40b71d731
komesergey <komesergey@gmail.com>
2018-07-09 07:06:16 +0300

    Some formatting done and additional measurements added

--- cuda.cpp
+++ cuda.cpp
@@ -14,7 +14,6 @@ static const char* source = "\textern \"C\" __global__ void vecAdd(int *a, int *
                             "\tc[id] = a[id] + b[id];\n"
                             "}\t";
 
-
 void checkCudaErrors(CUresult err) {
     if (err != CUDA_SUCCESS) {
         std::cout << err << std::endl;
@@ -33,30 +32,24 @@ int main(int argc,char *argv[])
     printf("%s\n", nvrtcGetErrorString(result));
     nvrtcResult compileResult =  nvrtcCompileProgram ( program, 0, nullptr);
     printf("%s\n", nvrtcGetErrorString(compileResult));
-
     size_t logSize;
     nvrtcGetProgramLogSize(program, &logSize);
     auto *log = new char[logSize];
     nvrtcGetProgramLog(program, log);
-
     size_t ptxSize;
     nvrtcGetPTXSize(program, &ptxSize);
     auto *ptx = new char[ptxSize];
     nvrtcGetPTX(program, ptx);
-
     printf("%s", ptx);
-
     CUdevice cuDevice;
     CUcontext cuContext;
     CUmodule cuModule;
     CUfunction function;
-
     checkCudaErrors(cuInit(0));
     checkCudaErrors(cuDeviceGet(&cuDevice, 0));
     checkCudaErrors(cuCtxCreate(&cuContext, 0, cuDevice));
     checkCudaErrors(cuModuleLoadDataEx( &cuModule , ptx , 0, nullptr, nullptr));
     checkCudaErrors(cuModuleGetFunction(&function, cuModule, "vecAdd"));
-
     auto *Host_a = (int*)malloc(sizeof(int)*N);
     auto *Host_b = (int*)malloc(sizeof(int)*N);
     auto *Host_c = (int*)malloc(sizeof(int)*N);
@@ -64,44 +57,31 @@ int main(int argc,char *argv[])
         Host_a[i] = i;
         Host_b[i] = N - i;
     }
-
-
+    auto startFull = std::chrono::high_resolution_clock::now();
     CUdeviceptr devBufferA;
     checkCudaErrors(cuMemAlloc(&devBufferA, sizeof(int) * N));
     checkCudaErrors(cuMemcpyHtoD(devBufferA, Host_a, sizeof(int) * N));
-
     CUdeviceptr devBufferB;
     checkCudaErrors(cuMemAlloc(&devBufferB, sizeof(int) * N));
     checkCudaErrors(cuMemcpyHtoD(devBufferB, Host_b, sizeof(int) * N));
-
     CUdeviceptr devBufferC;
     checkCudaErrors(cuMemAlloc(&devBufferC, sizeof(int) * N));
-
     void* KernelParams[] = {&devBufferA, &devBufferB, &devBufferC, &size};
-
     unsigned  int blockSize, gridSize;
-
     blockSize = 1024;
-
     gridSize = (unsigned int)ceil((float)N/blockSize);
-
     auto start = std::chrono::high_resolution_clock::now();
-
-    checkCudaErrors(cuLaunchKernel(function, gridSize, 1, 1, blockSize, 1, 1, 0,
-                                   nullptr, KernelParams, nullptr));
-
+    checkCudaErrors(cuLaunchKernel(function, gridSize, 1, 1, blockSize, 1, 1, 0, nullptr, KernelParams, nullptr));
     auto done = std::chrono::high_resolution_clock::now();
-
     checkCudaErrors(cuMemcpyDtoH(Host_c, devBufferC, N*sizeof(int)));
-
+    auto doneFull = std::chrono::high_resolution_clock::now();
     long resultTime = std::chrono::duration_cast<std::chrono::nanoseconds>(done - start).count();
-
-    printf("Time spent: %ld\n", resultTime);
-
+    long resultTimeFull = std::chrono::duration_cast<std::chrono::milliseconds>(doneFull - startFull).count();
+    printf("Time spent adding vector of size %d: %ld nanoseconds\n", N, resultTime);
+    printf("Time spent adding vector of size %d: %ld milliseconds (with copying data)\n", N, resultTimeFull);
     checkCudaErrors(cuMemFree(devBufferA));
     checkCudaErrors(cuMemFree(devBufferB));
     checkCudaErrors(cuMemFree(devBufferC));
     checkCudaErrors(cuModuleUnload(cuModule));
     checkCudaErrors(cuCtxDestroy(cuContext));
-
 }
--- main.cpp
+++ main.cpp
@@ -29,61 +29,30 @@ long testSpeed(cl_device_id current_id){
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    // Load the kernel source code into the array source_str
     const char *source_str = &source[0];
     size_t source_size;
     source_size = sizeof(source);
-
-    // Create an OpenCL context
     cl_context context = clCreateContext(nullptr, 1, &current_id, nullptr, nullptr, &ret);
-
-    // Create a command queue
     cl_command_queue command_queue = clCreateCommandQueue(context, current_id, 0, &ret);
-
-    // Create memory buffers on the device for each vector
-    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-                                      LIST_SIZE * sizeof(int), nullptr, &ret);
-    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-                                      LIST_SIZE * sizeof(int), nullptr, &ret);
-    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-                                      LIST_SIZE * sizeof(int), nullptr, &ret);
-
-    // Copy the lists A and B to their respective memory buffers
-    clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
-                         LIST_SIZE * sizeof(int), A, 0, nullptr, nullptr);
-    clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0,
-                         LIST_SIZE * sizeof(int), B, 0, nullptr, nullptr);
-
-    // Create a program from the kernel source
-    cl_program program = clCreateProgramWithSource(context, 1,
-                                                   (const char **)&source_str, (const size_t *)&source_size, &ret);
-
-    // Build the program
+    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int), nullptr, &ret);
+    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int), nullptr, &ret);
+    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, LIST_SIZE * sizeof(int), nullptr, &ret);
+    clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), A, 0, nullptr, nullptr);
+    clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), B, 0, nullptr, nullptr);
+    cl_program program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
     clBuildProgram(program, 1, &current_id, nullptr, nullptr, nullptr);
-
-    // Create the OpenCL kernel
     cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
-
-
-    // Set the arguments of the kernel
     clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
     clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
     clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
-
-    // Execute the OpenCL kernel on the list
-    size_t global_item_size = LIST_SIZE; // Process the entire lists
-    size_t local_item_size = 64; // Divide work items into groups of 64
-    clEnqueueNDRangeKernel(command_queue, kernel, 1, nullptr,
-                           &global_item_size, &local_item_size, 0, nullptr, nullptr);
-
-    // Read the memory buffer C on the device to the local variable C
+    size_t global_item_size = LIST_SIZE;
+    size_t local_item_size = 64;
+    clEnqueueNDRangeKernel(command_queue, kernel, 1, nullptr, &global_item_size, &local_item_size, 0, nullptr, nullptr);
     auto *C = (int*)malloc(sizeof(int)*LIST_SIZE);
-    clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0,
-                        LIST_SIZE * sizeof(int), C, 0, nullptr, nullptr);
+    clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), C, 0, nullptr, nullptr);
 
     auto done = std::chrono::high_resolution_clock::now();
 
-    // Clean up
     clFlush(command_queue);
     clFinish(command_queue);
     clReleaseKernel(kernel);
@@ -100,7 +69,6 @@ long testSpeed(cl_device_id current_id){
 }
 
 int main() {
-    // Create the two input vectors
     int i, j;
     char* value;
     size_t valueSize;
@@ -109,22 +77,15 @@ int main() {
     cl_uint deviceCount;
     cl_device_id* devices;
 
-    // get all platforms
     clGetPlatformIDs(0, nullptr, &platformCount);
     platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * platformCount);
     clGetPlatformIDs(platformCount, platforms, nullptr);
 
     for (i = 0; i < platformCount; i++) {
-
-        // get all devices
         clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, nullptr, &deviceCount);
         devices = (cl_device_id *) malloc(sizeof(cl_device_id) * deviceCount);
         clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, nullptr);
-
-        // for each device print critical attributes
         for (j = 0; j < deviceCount; j++) {
-
-            // print device name
             clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, nullptr, &valueSize);
             value = (char *) malloc(valueSize);
             clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, nullptr);
@@ -132,12 +93,10 @@ int main() {
             long time = testSpeed(devices[j]);
             printf("Time: %ld\n", time);
             free(value);
-
         }
         free(devices);
     }
 
     free(platforms);
-
     return 0;
 }
